926684685d065680acc3f8439550fa6eee95b04b
Max zhang <373884384@qq.com>
2020-10-15 12:06:29 +0800

    server酱推送

--- app.js
+++ app.js
@@ -11,28 +11,28 @@ const download = require('download')
 const KEY = process.env.JD_COOKIE
 const serverJ = process.env.PUSH_KEY
 
-async function downFile () {
-    // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
-    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
-    await download(url, './')
+async function downFile() {
+  // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
+  const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
+  await download(url, './')
 }
 
-async function changeFiele () {
-   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
-   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`)
-   await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
+async function changeFiele() {
+  let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
+  content = content.replace(/var Key = ''/, `var Key = '${KEY}'`)
+  await fs.writeFileSync('./JD_DailyBonus.js', content, 'utf8')
 }
 
-async function sendNotify (text,desp) {
-  const options ={
-    uri:  `https://sc.ftqq.com/${serverJ}.send`,
+async function sendNotify(text, desp) {
+  const options = {
+    uri: `https://sc.ftqq.com/${serverJ}.send`,
     form: { text, desp },
     json: true,
     method: 'POST'
   }
-  await rp.post(options).then(res=>{
+  await rp.post(options).then(res => {
     console.log(res)
-  }).catch((err)=>{
+  }).catch((err) => {
     console.log(err)
   })
 }
@@ -52,7 +52,7 @@ async function start() {
   await exec("node JD_DailyBonus.js >> result.txt");
   console.log('执行完毕')
 
-  if (serverJ) {
+  if (serverJ && serverJ.trim != '') {
     const path = "./result.txt";
     let content = "";
     if (fs.existsSync(path)) {
